d64c56cc75e6686ed3a2d9b962b483bdedfd6c80
Woeler <woeler@woeler.eu>
2022-10-21 21:33:49 +0200

    Test Chinese

--- tests/NewsTest.php
+++ tests/NewsTest.php
@@ -100,4 +100,20 @@ class NewsTest extends \PHPUnit\Framework\TestCase
         $this->assertNotNull($article->getDescription());
         $this->assertNotNull($article->getLink());
     }
+    
+    public function testFetchChinese()
+    {
+        $fetcher  = new NewsFetcher(NewsFetcher::LANG_CN);
+        $articles = $fetcher->fetchAll();
+
+        /** @var EsoArticle $article */
+        $article = $articles[0];
+
+        $this->assertTrue(count($articles) > 0);
+        $this->assertNotNull($article->getTitle());
+        $this->assertNotNull($article->getTimestamp());
+        $this->assertNotNull($article->getImage());
+        $this->assertNotNull($article->getDescription());
+        $this->assertNotNull($article->getLink());
+    }
 }
